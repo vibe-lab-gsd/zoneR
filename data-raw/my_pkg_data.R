9db1b848f5021dec965a99cb65e60bbe5dafde4b
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-07-17 15:20:48 -0600

    deleted some spaces

--- data-raw/my_pkg_data.R
+++ data-raw/my_pkg_data.R
@@ -1,8 +1,5 @@
 ## code to prepare `my_pkg_data` dataset goes here
 
-
-
-
 state_planes_crs <- sf::st_read("inst/extdata/sp_crs.geojson")
 
 usethis::use_data(state_planes_crs, overwrite = TRUE, internal = TRUE)
