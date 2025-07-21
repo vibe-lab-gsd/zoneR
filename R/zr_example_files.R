b42cad67e4b96eca5f7bc9dfbb5102dfa420798b
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-07-21 11:27:15 -0600

    created easy function to get example files

---
+++ R/zr_example_files.R
@@ -0,0 +1,23 @@
+#' Get path to zoneR's example files
+#'
+#' zoneR has a few example OZFS files stored internally. `zr_example_files`
+#' returns either a list of names of the example files, or the file path
+#' of a specified file.
+#'
+#' @param file If known, the name of the example file.
+#'
+#' @returns Either a list of names of the example files, or a path
+#' to the specified file
+#' @export
+#'
+#' @examples
+#' zr_example_files()
+#' zr_example_files("Paradise.parcel)
+#'
+zr_example_files <- function(file = NULL){
+  if (is.null(file)) {
+    dir(system.file("extdata", package = "zoneR"))[dir(system.file("extdata", package = "zoneR")) != "sp_crs.geojson"]
+  } else {
+    system.file("extdata", file, package = "zoneR", mustWork = TRUE)
+  }
+}
