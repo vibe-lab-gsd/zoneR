784ec3cfa8a3c538ac7a05e6ff40d9e8c03a5ac0
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-07-23 05:39:01 -0600

    added possible_checks data

--- data-raw/my_pkg_data.R
+++ data-raw/my_pkg_data.R
@@ -1,7 +1,39 @@
 ## code to prepare `my_pkg_data` dataset goes here
 
+# State Planes Data
 state_planes_crs <- sf::st_read("inst/extdata/sp_crs.geojson")
 
-usethis::use_data(state_planes_crs, overwrite = TRUE, internal = TRUE)
 usethis::use_data(state_planes_crs, overwrite = TRUE)
 
+
+# Possible Checks Variable
+possible_checks <- c("res_type",
+                     "far",
+                     "fl_area",
+                     "fl_area_first",
+                     "fl_area_top",
+                     "footprint",
+                     "height",
+                     "height_eave",
+                     "lot_cov_bldg",
+                     "lot_area",
+                     "parking_enclosed",
+                     "stories",
+                     "unit_0bed",
+                     "unit_1bed",
+                     "unit_2bed",
+                     "unit_3bed",
+                     "unit_4bed",
+                     "unit_density",
+                     "unit_pct_0bed",
+                     "unit_pct_1bed",
+                     "unit_pct_2bed",
+                     "unit_pct_3bed",
+                     "unit_pct_4bed",
+                     "total_units",
+                     "unit_size_avg",
+                     "unit_size",
+                     "bldg_fit",
+                     "overlay")
+
+usethis::use_data(possible_checks, overwrite = TRUE)
