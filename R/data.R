f995cfb7c5089e045eaa3201daee3916f27b30e5
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-07-23 05:39:36 -0600

    updated data documentation

--- R/data.R
+++ R/data.R
@@ -12,9 +12,46 @@
 #'   \item{ZONE}{Zone Abbreviation}
 #'   \item{ZONENAME}{Zone Name}
 #'   \item{EPSG_NAD83}{Projected CRS EPSG Code}
-#'   \item{geometry}{Zone Geometry}
 #'
 #' }
 #' @source <https://hub.arcgis.com/datasets/esri::usa-state-plane-zones-nad83/explore>
 #' @source <https://epsg.io/>
 "state_planes_crs"
+
+#' Possible Constraints to Check
+#'
+#' This character vector lists all the possible constraints that can be
+#' checked in the `zr_run_zoning_checks()` functions
+#'
+#' @format
+#' A character vector
+#' c("res_type",
+#' "far",
+#' "fl_area",
+#' "fl_area_first",
+#' "fl_area_top",
+#' "footprint",
+#' "height",
+#' "height_eave",
+#' "lot_cov_bldg",
+#' "lot_area",
+#' "parking_enclosed",
+#' "stories",
+#' "unit_0bed",
+#' "unit_1bed",
+#' "unit_2bed",
+#' "unit_3bed",
+#' "unit_4bed",
+#' "unit_density",
+#' "unit_pct_0bed",
+#' "unit_pct_1bed",
+#' "unit_pct_2bed",
+#' "unit_pct_3bed",
+#' "unit_pct_4bed",
+#' "total_units",
+#' "unit_size_avg",
+#' "unit_size",
+#' "bldg_fit",
+#' "overlay")
+#
+"possible_checks"
