6d8b173b1de04c17d94cb57cbee126adb03c435f
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-07-23 23:58:36 -0600

    Can now run with blank zoning data

--- R/zr_find_district_idx.R
+++ R/zr_find_district_idx.R
@@ -2,22 +2,27 @@
 #'
 #' @description
 #' `zr_find_district_idx()` uses [sf::st_join()] to adds a new column to the parcels data that
-#' contains the tidyzoning row index of the district the parcel is in
+#' contains the zoning_sf row index of the district the parcel is in
 #'
 #' @param parcels_centroids_sf An sf object with the parcel centroids
-#' @param tidyzoning A simple features object with a row for each zoning district and columns holding the geojson formatted zoning requirements of each district.
+#' @param zoning_sf A simple features object with a row for each zoning district and columns holding the geojson formatted zoning requirements of each district.
 #' @param idx_col_name The name of the new column that will be added with the district index
 #'
 #' @return The same parcels centroids object that was input but with an additional
-#' column stating the row number of the corresponding tidyzoning district
+#' column stating the row number of the corresponding zoning_sf district
 #' @export
 #'
-zr_find_district_idx <- function(parcels_centroids_sf, tidyzoning, idx_col_name = "zoning_id"){
+zr_find_district_idx <- function(parcels_centroids_sf, zoning_sf, idx_col_name = "zoning_id"){
 
-  tidyzoning <- sf::st_make_valid(tidyzoning)
-  tidyzoning[[idx_col_name]] <- 1:nrow(tidyzoning)
+  zoning_sf <- sf::st_make_valid(zoning_sf)
 
-  parcels_with_zoning_id <- sf::st_join(sf::st_make_valid(parcels_centroids_sf), tidyzoning[idx_col_name])
+  if (nrow(zoning_sf) > 0){
+    zoning_sf[[idx_col_name]] <- 1:nrow(zoning_sf)
+  } else{
+    parcels_centroids_sf[[idx_col_name]] <- NA
+    return(parcels_centroids_sf)
+  }
+  parcels_with_zoning_id <- sf::st_join(sf::st_make_valid(parcels_centroids_sf), zoning_sf[idx_col_name])
 
   return(parcels_with_zoning_id)
 
