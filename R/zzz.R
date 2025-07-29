c4a7d2620225d61004141becc772640f6a4d3a14
Carole Turley Voulgaris <64663478+c-voulgaris@users.noreply.github.com>
2025-07-29 15:39:06 -0400

    Update zzz.R
    Edited package load message.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("WARNING: The authors of this package are not attorneys and do not guarantee the results of this package.")
+  packageStartupMessage("None of the results of any of the functions in this package are legal advice. We do not guarantee any of it. You should definitely talk to an actual professional of some kind before you start building anything.")
 }
