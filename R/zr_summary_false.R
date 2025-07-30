4a09c6ca367264faa94fbb9cf711ec3a70535259
c-voulgaris <cvoulgaris@gsd.harvard.edu>
2025-07-30 16:18:44 -0400

    Added functions to summarize maybes and falses

---
+++ R/zr_summary_false.R
@@ -0,0 +1,28 @@
+#' Summarize the reasons for maybe results
+#'
+#' `zr_summary_by_muni()` summarizes results reasons given for maybe results from
+#' the output of zr_run_zoning_checks()
+#'
+#' @param detailed_result The output simple features data frame from
+#' zr_run_zoning_checks()
+#'
+#' @returns A tibble summarizing the reasons for maybe values.
+#'
+#' @export
+#'
+#' @examples
+zr_summary_false <- function(detailed_result){
+
+  summary <- detailed_result$reason[detailed_result$allowed == "FALSE"] |>
+    strsplit(", ") |>
+    unlist() |>
+    table() |>
+    as.data.frame() |>
+    arrange(-Freq) |>
+    mutate(`Reason for prohibition` =
+             reasons$false_explain[match(Var1, reasons$short_reason)]) |>
+    rename(`Number of parcels` = Freq) |>
+    select(`Reason for prohibition`, `Number of parcels`)
+
+  return(summary)
+}
