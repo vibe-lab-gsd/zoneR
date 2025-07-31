979a0f00ad07859d68b48edc340bf882f5cff55d
c-voulgaris <cvoulgaris@gsd.harvard.edu>
2025-07-31 10:10:52 -0400

    Added option to filter by municipality

--- R/zr_summary_maybe.R
+++ R/zr_summary_maybe.R
@@ -5,13 +5,21 @@
 #'
 #' @param detailed_result The output simple features data frame from
 #' zr_run_zoning_checks()
+#' @param muni The city to summarize data for. Defaults to "All" to aggregate
+#' across all cities include in the results file.
 #'
 #' @returns A tibble summarizing the reasons for maybe values.
 #'
 #' @export
 #'
 #' @examples
-zr_summary_maybe <- function(detailed_result){
+zr_summary_maybe <- function(detailed_result,
+                             muni = "All"){
+
+  if(muni != "All") {
+    detailed_result <- detailed_result |>
+      filter(muni_name == muni)
+  }
 
   summary <- detailed_result$reason[detailed_result$allowed == "MAYBE"] |>
     strsplit(", ") |>
